7fd7949e989c2a85fef64d7bfca9b8b7da2068eb
hamzaalaa-54 <henkshalaa54@gmail.com>
2020-03-17 16:13:01 +0200

    who will won the million

---
+++ Q10.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cout<<"               Who is the best programmer in Egypt?"<<endl<<"1) eslam elkhafagy                "<<"2) eslam elkhafagy "<<endl<<"3) eslam elkhafagy                "<<"4) eslam elkhafagy"<<endl;
+	cin>>n;
+	switch(n)
+	{
+		case 1:cout<<"correct answer";
+		break;
+        case 2:cout<<"correct answer";
+        break;
+       	case 3:cout<<"correct answer";
+       	break;
+       	case 4 :cout<<"correct answer";
+	}
+	
+}
